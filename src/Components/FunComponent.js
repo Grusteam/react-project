828419651680f9d7cee8631504d1b70af001cbdc
Serge <sergeuniquite@gmail.com>
2020-11-24 20:36:30 +0300

    1

--- src/Components/FunComponent.js
+++ src/Components/FunComponent.js
@@ -1,10 +1,27 @@
 import React  from 'react';
 import PC  from './PureComponent.tsx';
 
+
+let wasChanged = false
+
 export default function FunComponent(props = {}) {
 
     const [count, changeCount] = React.useState(0)
     const [runned, setRunned] = React.useState(false)
+    
+    
+    const [test1, setTest1] = React.useState('init1')
+    const [test2, setTest2] = React.useState('init2')
+
+    const wrapInProxy = (anyHook, anyVal) => {
+        console.log('wasChanged pre', wasChanged);
+
+        anyHook(anyVal)
+        wasChanged = true
+
+        console.log('wasChanged post', wasChanged);
+    }
+
 
     React.useEffect(() => {
         // console.log('runned', runned);
@@ -28,6 +45,8 @@ export default function FunComponent(props = {}) {
     return <div>
         <p>FunComponent</p>
         <div>count: {count}</div>
+        <input type="text" value={test1} onChange={e => wrapInProxy(setTest1, e.target.value)}/>
+        <input type="text" value={test2} onChange={e => wrapInProxy(setTest2, e.target.value)}/>
         <button onClick={run}>run</button>
         <button onClick={() => changeCount(count + 100500)}>plus 100500</button>
         <div>{props.prop}</div>
